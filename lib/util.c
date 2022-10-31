cc67aea1276fce173e5bc7149152fb60648c7bf9
TiagoLacerda <tiagomlbrasil@gmail.com>
2022-10-31 19:51:54 -0300

    Added utility lib

---
+++ lib/util.c
@@ -0,0 +1,99 @@
+#ifndef UTIL_H
+#define UTIL_H
+#include "util.h"
+#endif
+
+#ifndef STDLIB_H
+#define STDLIB_H
+#include <stdlib.h>
+#endif
+
+#ifndef STDIO_H
+#define STDIO_H
+#include <stdio.h>
+#endif
+
+#ifndef TIME_H
+#define TIME_H
+#include <time.h>
+#endif
+
+void swap(double *a, double *b)
+{
+    double temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+double *random_double_array(int length, double min, double max)
+{
+    double range = max - min, *array = (double *)malloc(sizeof(double) * length);
+
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = min + (double)(rand() * (range / RAND_MAX));
+    }
+
+    return array;
+}
+
+void read_double_array(double **array, int *length, char *path)
+{
+    FILE *file = fopen(path, "r");
+
+    fscanf(file, "%d", length);
+
+    *array = (double *)malloc(*length * sizeof(double));
+    
+    for (int i = 0; i < *length; i++)
+    {
+        fscanf(file, "%lf", &(*array)[i]);
+    }
+
+    fclose(file);
+}
+
+void save_double_array(double *array, int length, char *path)
+{
+    FILE *file = fopen(path, "w");
+
+    fprintf(file, "%d\n", length);
+    for (int i = 0; i < length; i++)
+    {
+        fprintf(file, "%.15lf\n", array[i]);
+    }
+
+    fclose(file);
+}
+
+void print_double_array(double *array, int length)
+{
+    if (length < 1)
+    {
+        printf("[]\n");
+    }
+    else
+    {
+        printf("[");
+
+        for (int i = 0; i < length - 1; i++)
+        {
+            printf("%lf, ", array[i]);
+        }
+
+        printf("%lf]\n", array[length - 1]);
+    }
+}
+
+/// @brief Returns index of first element out of order in [array]. Returns -1 if [array] is sorted.
+int sorted(double *array, int length)
+{
+    for (int i = 0; i < length - 1; i++)
+    {
+        if (array[i + 1] < array[i])
+        {
+            return i;
+        }
+    }
+    return -1;
+}
---
+++ lib/util.h
@@ -0,0 +1,22 @@
+void swap(double *a, double *b);
+
+/// @brief Create a random array with [length] values ranging from [min] to [max].
+double *random_double_array(int length, double min, double max);
+
+/// @brief Reads an [array] of doubles and it's [length] from [path].
+/// @param array An array of <double>.
+/// @param length Length of [array].
+/// @param path Path to file, containing filename and extension.
+void read_double_array(double **array, int *length, char *path);
+
+/// @brief Writes an [array] of doubles preceded by it's [length] to [path].
+/// @param array An array of <double>.
+/// @param length Length of [array].
+/// @param path Path to file, containing filename and extension.
+void save_double_array(double *array, int length, char *path);
+
+/// @brief Prints [array] to the console.
+void print_double_array(double *array, int length);
+
+/// @brief Returns index of first element out of order in [array]. Returns -1 if [array] is sorted.
+int sorted(double *array, int length);
